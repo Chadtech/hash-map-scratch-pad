e976f390a0447b8dc4fc35fc47e796e6d4dfcaf5
Chad Stearns <chadtech0@gmail.com>
2022-01-31 16:46:56 -0500

    Rearranged some things

--- src/HashMap.elm
+++ src/HashMap.elm
@@ -1,15 +1,50 @@
 module HashMap exposing (HashMap)
 
 import Array exposing (Array)
+import Dict exposing (Dict)
 
 
+title =
+    """
+   ▄█    █▄       ▄████████    ▄████████    ▄█    █▄         
+  ███    ███     ███    ███   ███    ███   ███    ███        
+  ███    ███     ███    ███   ███    █▀    ███    ███        
+ ▄███▄▄▄▄███▄▄   ███    ███   ███         ▄███▄▄▄▄███▄▄      
+▀▀███▀▀▀▀███▀  ▀███████████ ▀███████████ ▀▀███▀▀▀▀███▀       
+  ███    ███     ███    ███          ███   ███    ███        
+  ███    ███     ███    ███    ▄█    ███   ███    ███        
+  ███    █▀      ███    █▀   ▄████████▀    ███    █▀         
+                                                             
+   ▄▄▄▄███▄▄▄▄      ▄████████    ▄███████▄    ▄████████      
+ ▄██▀▀▀███▀▀▀██▄   ███    ███   ███    ███   ███    ███      
+ ███   ███   ███   ███    ███   ███    ███   ███    █▀       
+ ███   ███   ███   ███    ███   ███    ███   ███             
+ ███   ███   ███ ▀███████████ ▀█████████▀  ▀███████████      
+ ███   ███   ███   ███    ███   ███                 ███      
+ ███   ███   ███   ███    ███   ███           ▄█    ███      
+  ▀█   ███   █▀    ███    █▀   ▄████▀       ▄████████▀    
+
+    """
+
+
+users : Dict String User
+users =
+    Dict.empty
+
+
+{-|
+
+    usersHashMap : HashMap String User
+
+-}
 thesis =
     """
 HashMaps are like arrays, where an element’s index
 is determined by passing the key into a function.
 
 We turn a key into an index by a “Hash function”,
-a function that transforms any kind of data into a number.
+a function that transforms any kind of data into a
+number.
     """
 
 
@@ -75,10 +110,16 @@ type Color
 
 {-|
 
-    Lets add `Yellow` under the key "Chad". Since the hashmap is currently
-    empty, we need to allocate some space for it's first entry. Lets add a
-    "level" to the hashmap, which we arbitrarily picked 1 level = 4 slots
-    long..
+    Lets add `Yellow` under the key "Chad".
+
+    Suppose "Chad" hashes to `2`, meaning we want to
+    add "Chad" => Yellow to index 2 in the HashMap
+
+    We have a problem! The hashmap is currently empty,
+    and there is no index 2. We need to allocate
+    some space for it's first entry. Lets add a "level"
+    to the hashmap, which we arbitrarily picked
+    1 level = 4 slots long..
 
 -}
 s1 =
@@ -91,8 +132,9 @@ s1 =
 
 {-|
 
-    so suppose "Chad" hashes to `2`, if we insert that entry at index 2
-    our HashMap becomes..
+    Now there is enough space to insert
+
+    "Chad" => Yellow
 
 -}
 s2 =
@@ -105,8 +147,8 @@ s2 =
 
 {-|
 
-    Lets add `OliveDrab` under the key "Becky", suppose "Becky" hashes to `1`
-    our HashMap becomes..
+    Lets add `OliveDrab` under the key "Becky", suppose
+    "Becky" hashes to `1` our HashMap becomes..
 
 -}
 s3 =
@@ -119,11 +161,12 @@ s3 =
 
 {-|
 
-    Lets add `Red` under the key "Walter", suppose "Walter" hashes to `2`
-    our HashMap becomes..
-    Uh oh! Index `2` is already taken. We need more space in our HashMap.
-    So lets grow the HashMap from 1 level to 2 levels, which is another
-    way of saying grow it from 4 slots to 8 slots..
+    Lets add `Red` under the key "Walter", suppose "Walter"
+    hashes to `2` our HashMap becomes..
+    Uh oh! Index `2` is already taken. We need more space
+    in our HashMap. So lets grow the HashMap from 1 level
+    to 2 levels, which is another way of saying grow it
+    from 4 slots to 8 slots..
 
 -}
 s4 =
@@ -140,9 +183,10 @@ s4 =
 
 {-|
 
-     ..and then add "Walter" to the next level in the HashMap (indices 4
-     through 7). Say in the next level "Walter" hashes to `0`, then we add
-     "Walter" to index 4, because 4 = 1 * levelSize + 0.
+     ..and then add "Walter" to the next level in the
+     HashMap (indices 4 through 7). Say in the next
+     level "Walter" hashes to `0`, then we add "Walter"
+     to index 4, because 4 = 1 * levelSize + 0.
 
 -}
 s5 =
@@ -159,8 +203,9 @@ s5 =
 
 {-|
 
-     Now say we want to retrieve Walter's favorite color. We first hash
-     "Walter", which gives us `2`, and then we get the entry at index 2..
+     Now say we want to retrieve Walter's favorite color.
+     We first hash "Walter", which gives us `2`, and then
+     we get the entry at index 2..
 
 -}
 s6 =
@@ -169,14 +214,15 @@ s6 =
 
 {-|
 
-     ..but the entry at `2` is stored under the key "Chad", which is
-      not what we are looking for, so we have to move on and search
-      for "Walter" in the next level of this HashMap..
+     ..but the entry at `2` is stored under the key "Chad",
+      which is not what we are looking for, so we have to
+      move on and search for "Walter" in the next level of
+      this HashMap..
 
-      In the next level we rehash "Walter", which hashes to "0", and
-      we get index 4, because 4 = 1 * levelSize + 0. This entry
-      does have the key "Walter" so we know we found the correct
-      entry.
+      In the next level we rehash "Walter", which hashes to
+      "0", and we get index 4, because 4 = 1 * levelSize + 0.
+      This entry does have the key "Walter" so we know we
+      found the correct entry.
 
 -}
 s7 =
@@ -185,22 +231,16 @@ s7 =
 
 {-|
 
-     Suppose we want to look up Katerina's favorite color, who is
-     absent from our HashMap. We hash "Katerina" to `0`, and we
-     look up that slot to find `Empty`, so we conclude Katerina's
-     favorite color  is not stored in this HashMap.
+     Lets delete Chad's favorite color from the HashMap
 
-     Reaching `Empty` leads us to stop our search.
-
-     If "Katerina" hashed to `1` instead, we would check if it matches
-     the key at that slot "Becky", see that it doesnt, and move onto
-     check the next level in the same way we checked the first.
+     We hash "Chad" to `2`, check the key at 2 to make sure
+     its "Chad", and then we replace the whole entry with `Tombstone`.
 
 -}
-s9 =
+s8 =
     [ Empty
     , Entry "Becky" OliveDrab
-    , Entry "Chad" Yellow
+    , Tombstone
     , Empty
     , Entry "Walter" Red
     , Empty
@@ -211,13 +251,27 @@ s9 =
 
 {-|
 
-     Lets delete Chad's favorite color from the HashMap
+     Suppose we want to look up Katerina's favorite color,
+     who is absent from our HashMap. We hash "Katerina" to
+     `0`, and we look up that slot to find `Empty`, so we
+     conclude Katerina's favorite color  is not stored
+     in this HashMap.
 
-     We hash "Chad" to `2`, check the key at 2 to make sure its "Chad",
-     and then we replace the whole entry with `Tombstone`.
+     Reaching `Empty` leads us to stop our search.
+
+     If "Katerina" hashed to `1` instead, we would check
+     if it matches the key at that slot "Becky", see that
+     it doesnt, and move onto check the next level in the
+     same way we checked the first.
+
+     If "Katerina" hashed to `2` instead, we would look up
+     index 2 and find Tombstone. Since the value is
+     Tombstone, there was once a value, there, and there
+     may have been a collision with that value, and
+     "Katerina" may be in a subsequent level
 
 -}
-s8 =
+s9 =
     [ Empty
     , Entry "Becky" OliveDrab
     , Tombstone
@@ -234,8 +288,8 @@ levelSize =
     4
 
 
-hash : a -> Int
-hash value =
+hash : a -> Int -> Int
+hash value seed =
     --
     -- Nevermind how this is implemented..
     --
@@ -280,3 +334,7 @@ hash value =
             thesis
     in
     4
+
+
+type User
+    = User
